1911398e62c8da083891cf0a6d67837d6bb2f9f1
JyggalagIRL <jyggalag123@gmail.com>
2017-07-24 18:41:06 +0800

    Fix refresh footer not appear when calling noticeNoMoreData() after first page reloaded.

--- Sources/ESPullToRefresh.swift
+++ Sources/ESPullToRefresh.swift
@@ -440,7 +440,7 @@ open class ESRefreshFooterView: ESRefreshComponent {
             return
         }
 
-        if scrollView.contentSize.height <= 0.0 || scrollView.contentOffset.y + scrollView.contentInset.top <= 0.0 {
+        if scrollView.contentSize.height == 0.0 || scrollView.contentOffset.y + scrollView.contentInset.top < 0.0 {
             self.alpha = 0.0
             return
         } else {
